dfc4404f3b88c6e7441fb17be7651ef11b7e5a6e
Sojib Sheikh SR <sheikhsr.sojib@gmail.com>
2021-11-25 09:19:48 +0600

    Connect Mongodb Database

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(cors)
+app.use(morgan('dev'));
+
+const url = process.env.DB_URL;
+mongoose.connect(url,{useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Database is connected,Yeahooooo!!!');
+    })
+    .catch((err)=>{
+        console.log(err.message);
+    })
+
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({
+        status:true,
+        "message":"Book Shop Rest"
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`server running at the port : ${port}`)
+})
